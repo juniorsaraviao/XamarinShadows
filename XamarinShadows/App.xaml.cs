3e6a5e33940a932f1a05556c341157737ba2c86b
Junior Saravia <junior.saravia.o@uni.pe>
2022-11-25 17:12:09 -0500

    Initial commit

---
+++ XamarinShadows/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinShadows
+{
+   public partial class App : Application
+   {
+      public App()
+      {
+         InitializeComponent();
+
+         //Sharpnado.Shades.Initializer.Initialize(loggerEnable: false);
+
+         MainPage = new MainPage();
+      }
+
+      protected override void OnStart()
+      {
+      }
+
+      protected override void OnSleep()
+      {
+      }
+
+      protected override void OnResume()
+      {
+      }
+   }
+}
