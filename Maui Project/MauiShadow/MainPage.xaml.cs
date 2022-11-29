1fdd82d75a92cdf4c2f822a8dcd658be0f3a5f0e
Junior Saravia <junior.saravia.o@uni.pe>
2022-11-28 20:08:09 -0500

    Move code

---
+++ Maui Project/MauiShadow/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiShadow;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Maui Project/MauiShadow/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiShadow;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
